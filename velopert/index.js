9f9298b62ab99594bab3c69f6b4dd9e49ac0ce33
Lee-Byung-Kyu <leebang54@khu.ac.kr>
2022-07-14 18:04:55 +0900

    [init] velopert

--- velopert/index.js
+++
@@ -1,7 +0,0 @@
-const number = document.getElementById("number");
-const increase = document.getElementById("increase");
-const decrease = document.getElementById("decrease");
-
-console.log(number);
-console.log(increase);
-console.log(decrease);
